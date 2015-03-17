75e682ce0dc0bd107f210bfd06159b1426a1303b
xieliang <xieliang@xiaomi.com>
2015-03-17 10:02:43 +0800

    port HBASE-10205
    Summary: Fixes T3593
    
    Test Plan: N/A
    
    Reviewers: fenghonghua, liushaohui, cuijianwei, heliangliang, chengqiming, zhouyingchao
    
    Reviewed By: zhouyingchao
    
    Maniphest Tasks: T3593
    
    Differential Revision: https://phabricator.d.xiaomi.net/D11268

--- src/main/java/org/apache/hadoop/hbase/io/hfile/bucket/BucketAllocator.java
+++ src/main/java/org/apache/hadoop/hbase/io/hfile/bucket/BucketAllocator.java
@@ -20,7 +20,7 @@
 
 package org.apache.hadoop.hbase.io.hfile.bucket;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
@@ -29,7 +29,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.hbase.io.hfile.BlockCacheKey;
-import org.apache.hadoop.hbase.io.hfile.CacheConfig;
 import org.apache.hadoop.hbase.io.hfile.bucket.BucketCache.BucketEntry;
 
 /**
@@ -169,13 +168,13 @@ public final class BucketAllocator {
     private int sizeIndex;
 
     BucketSizeInfo(int sizeIndex) {
-      bucketList = new ArrayList<Bucket>();
-      freeBuckets = new ArrayList<Bucket>();
-      completelyFreeBuckets = new ArrayList<Bucket>();
+      bucketList = new LinkedList<Bucket>();
+      freeBuckets = new LinkedList<Bucket>();
+      completelyFreeBuckets = new LinkedList<Bucket>();
       this.sizeIndex = sizeIndex;
     }
 
-    public void instantiateBucket(Bucket b) {
+    public synchronized void instantiateBucket(Bucket b) {
       assert b.isUninstantiated() || b.isCompletelyFree();
       b.reconfigure(sizeIndex);
       bucketList.add(b);
@@ -225,7 +224,7 @@ public final class BucketAllocator {
       return b;
     }
 
-    private void removeBucket(Bucket b) {
+    private synchronized void removeBucket(Bucket b) {
       assert b.isCompletelyFree();
       bucketList.remove(b);
       freeBuckets.remove(b);
@@ -241,7 +240,7 @@ public final class BucketAllocator {
       if (b.isCompletelyFree()) completelyFreeBuckets.add(b);
     }
 
-    public IndexStatistics statistics() {
+    public synchronized IndexStatistics statistics() {
       long free = 0, used = 0;
       for (Bucket b : bucketList) {
         free += b.freeCount();
