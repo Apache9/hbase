eefea63e8f751506479027b0cdc87382ddcfa940
cuijianwei <cuijianwei@cuijianwei-OptiPlex-990.(none)>
2015-07-16 11:22:29 +0800

    port D1173: support CompareOp.NOT_EQUAL in checkAndMutate when cell is null or value is null

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -2806,19 +2806,39 @@ public class HRegion implements HeapSize { // , Writable{
           }
         }
         List<Cell> result = get(get, false);
+        
+        // result.size() must be 0 or 1, except that the default max version
+        // setting logic of Get changed.
+        if (result.size() > 1) {
+          throw new RuntimeException(
+              "Result size of get in checkAndMutate must be 0 or 1, actual size:" + result.size());
+        }
+        
         if (this.rsServices != null) {
           this.rsServices.getRegionServerQuotaManager().grabQuota(this, Result.create(result));
         }
 
         boolean valueIsNull = comparator.getValue() == null ||
           comparator.getValue().length == 0;
+        boolean rowIsNull = (result.size() == 0 || result.get(0).getValue().length == 0);
         boolean matches = false;
-        if (result.size() == 0 && valueIsNull) {
-          matches = true;
-        } else if (result.size() > 0 && result.get(0).getValueLength() == 0 &&
-            valueIsNull) {
-          matches = true;
-        } else if (result.size() == 1 && !valueIsNull) {
+        if (rowIsNull || valueIsNull) {
+          if (compareOp.equals(CompareOp.EQUAL)) {
+            matches = (rowIsNull == valueIsNull);
+          } else if (compareOp.equals(CompareOp.NOT_EQUAL)) {
+            matches = (rowIsNull != valueIsNull);
+          } else if (compareOp.equals(CompareOp.LESS) ||
+              compareOp.equals(CompareOp.LESS_OR_EQUAL) ||
+              compareOp.equals(CompareOp.GREATER) ||
+              compareOp.equals(CompareOp.GREATER_OR_EQUAL)) {
+            LOG.warn("CompareOp : "
+                + compareOp
+                + " is not supportted when cell value or comparator value is null, actual rowIsNull : "
+                + rowIsNull + ", valueIsNull : " + valueIsNull);
+          } else {
+            throw new DoNotRetryIOException("Unknown Compare op " + compareOp.name());
+          }
+        } else {
           Cell kv = result.get(0);
           int compareResult = comparator.compareTo(kv.getValueArray(),
               kv.getValueOffset(), kv.getValueLength());
@@ -2893,19 +2913,39 @@ public class HRegion implements HeapSize { // , Writable{
       mvcc.completeMemstoreInsert(mvcc.beginMemstoreInsert());
       try {
         List<Cell> result = get(get, false);
+        
+        // result.size() must be 0 or 1, except that the default max version
+        // setting logic of Get changed.
+        if (result.size() > 1) {
+          throw new RuntimeException(
+              "Result size of get in checkAndMutate must be 0 or 1, actual size:" + result.size());
+        }
+        
         if (this.rsServices != null) {
           this.rsServices.getRegionServerQuotaManager().grabQuota(this, Result.create(result));
         }
 
         boolean valueIsNull = comparator.getValue() == null ||
             comparator.getValue().length == 0;
+        boolean rowIsNull = (result.size() == 0 || result.get(0).getValue().length == 0);
         boolean matches = false;
-        if (result.size() == 0 && valueIsNull) {
-          matches = true;
-        } else if (result.size() > 0 && result.get(0).getValueLength() == 0 &&
-            valueIsNull) {
-          matches = true;
-        } else if (result.size() == 1 && !valueIsNull) {
+        if (rowIsNull || valueIsNull) {
+          if (compareOp.equals(CompareOp.EQUAL)) {
+            matches = (rowIsNull == valueIsNull);
+          } else if (compareOp.equals(CompareOp.NOT_EQUAL)) {
+            matches = (rowIsNull != valueIsNull);
+          } else if (compareOp.equals(CompareOp.LESS) ||
+              compareOp.equals(CompareOp.LESS_OR_EQUAL) ||
+              compareOp.equals(CompareOp.GREATER) ||
+              compareOp.equals(CompareOp.GREATER_OR_EQUAL)) {
+            LOG.warn("CompareOp : "
+                + compareOp
+                + " is not supportted when cell value or comparator value is null, actual rowIsNull : "
+                + rowIsNull + ", valueIsNull : " + valueIsNull);
+          } else {
+            throw new DoNotRetryIOException("Unknown Compare op " + compareOp.name());
+          }
+        } else {
           Cell kv = result.get(0);
           int compareResult = comparator.compareTo(kv.getValueArray(),
               kv.getValueOffset(), kv.getValueLength());
