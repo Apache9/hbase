30fc3ad2894fd40947567e0d14f1ef875fa9911c
cuijianwei <cuijianwei@cuijianwei-OptiPlex-990.(none)>
2015-07-20 19:29:03 +0800

    port D2355: trigger a thread dump before oom die

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegionServer.java
@@ -2941,6 +2941,7 @@ public class HRegionServer implements ClientProtos.ClientService.BlockingInterfa
         stop = true;
         LOG.fatal(
           "Run out of memory; HRegionServer will abort itself immediately", e);
+        ReflectionUtils.logThreadInfo(LOG, "thread dump from JvmThreadMonitor", 1000);
       }
     } finally {
       if (stop) {
