50de86ca3849bf9e991119027ba91118b59eb334
cuijianwei <cuijianwei@cuijianwei-OptiPlex-990.(none)>
2015-07-30 15:26:27 +0800

    increase reference count after add reference member for HRegion
    Summary: increase reference count after add reference member for HRegion
    
    Test Plan: N/A
    
    Reviewers: zhangguanghao1
    
    Differential Revision: https://phabricator.d.xiaomi.net/D18247

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -5880,7 +5880,7 @@ public class HRegion implements HeapSize { // , Writable{
   public static final long FIXED_OVERHEAD = ClassSize.align(
       ClassSize.OBJECT +
       ClassSize.ARRAY +
-      42 * ClassSize.REFERENCE + 2 * Bytes.SIZEOF_INT +
+      45 * ClassSize.REFERENCE + 2 * Bytes.SIZEOF_INT +
       (12 * Bytes.SIZEOF_LONG) +
       5 * Bytes.SIZEOF_BOOLEAN);
 
