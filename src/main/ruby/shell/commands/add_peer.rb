a3cd9ae5208c6b429ef0aa08f237f42fd352ca16
zhangguanghao1 <zhangguanghao1@xiaomi.com>
2015-09-21 16:42:42 +0800

    update the describe for set_peer_bandwidth
    Summary:
    update the describe for set_peer_bandwidth
    主要是为了方便使用
    1. set_peer_bandwidth的bandwidth类型是long
    2. 说明了下bandwidth是per rs的
    3. 添加了个add_peer使用name service的例子
    
    Test Plan: N/A
    
    Reviewers: chengqiming, cuijianwei
    
    Reviewed By: cuijianwei
    
    Differential Revision: https://phabricator.d.xiaomi.net/D21411

--- src/main/ruby/shell/commands/add_peer.rb
+++ src/main/ruby/shell/commands/add_peer.rb
@@ -35,6 +35,7 @@ Examples:
   hbase> add_peer '4', "zk7,zk8,zk9:11000:/hbase-proc", "DISABLED"
   hbase> add_peer '3', "zk4,zk5,zk6:11000:/hbase-test", "ENABLED", "tab1;tab2:cf1;tab3:cf2,cf3"
   hbase> add_peer '5', "zk4,zk5,zk6:11000:/hbase-test", "ENABLED", "tab1;tab2:cf1;tab3:cf2,cf3", 1048576
+  hbase> add_peer '6', "hbase://hbase-prc", "ENABLED", "tab1;tab2:cf1;tab3:cf2,cf3", 102400
 
 EOF
       end
