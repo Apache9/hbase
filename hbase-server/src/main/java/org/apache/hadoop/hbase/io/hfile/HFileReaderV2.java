0fe8635ac37cbd531bbc4074d04fb276a0688b72
zhangguanghao1 <zhangguanghao1@xiaomi.com>
2015-12-28 18:34:44 +0800

    Revert "bump version to 0.98.11-mdh2.0.7"
    This reverts commit 5f14e1ea75d12d9b50a1e76c2fc60966448b0eed.

--- hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/HFileReaderV2.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/io/hfile/HFileReaderV2.java
@@ -64,8 +64,7 @@ public class HFileReaderV2 extends AbstractHFileReader {
   public static final int MINOR_VERSION_NO_CHECKSUM = 0;
 
   /** HFile minor version that introduced pbuf filetrailer */
-  // currently, the minor version of xiaomi hbase 0.94 is 2
-  public static final int PBUF_TRAILER_MINOR_VERSION = 3;
+  public static final int PBUF_TRAILER_MINOR_VERSION = 2;
 
   /**
    * The size of a (key length, value length) tuple that prefixes each entry in
