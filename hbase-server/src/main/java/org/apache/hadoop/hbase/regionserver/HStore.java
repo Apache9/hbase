d37412e4d35f32faaca44bdf0255a88a2295efef
zhangduo <zhangduo@apache.org>
2016-04-11 14:55:33 +0800

    Backport HBASE-15400 Use DateTieredCompactor for Date Tiered Compaction
    Summary: HBASE-15400 Use DateTieredCompactor for Date Tiered Compaction
    
    Test Plan: UT
    
    Reviewers: cuijianwei, zhangguanghao1, yangzhe5, heliangliang
    
    Reviewed By: heliangliang
    
    Subscribers: infra-dev
    
    Differential Revision: https://phabricator.d.xiaomi.net/D31614

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HStore.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HStore.java
@@ -1379,7 +1379,7 @@ public class HStore implements Store {
         return false;
       }
     }
-    return storeEngine.getCompactionPolicy().isMajorCompaction(
+    return storeEngine.getCompactionPolicy().shouldPerformMajorCompaction(
         this.storeEngine.getStoreFileManager().getStorefiles());
   }
 
