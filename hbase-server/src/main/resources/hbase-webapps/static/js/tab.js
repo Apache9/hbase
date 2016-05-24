cd9edb031b6f09eb79ea6b8f91116647a76920a5
zhangguanghao1 <zhangguanghao1@xiaomi.com>
2016-05-24 19:04:33 +0800

    add show more link and warn for region number
    Summary: add show more link and warn for region number
    
    Test Plan: N/A
    
    Reviewers: cuijianwei
    
    Reviewed By: cuijianwei
    
    Maniphest Tasks: T5969
    
    Differential Revision: https://phabricator.d.xiaomi.net/D33712

--- hbase-server/src/main/resources/hbase-webapps/static/js/tab.js
+++ hbase-server/src/main/resources/hbase-webapps/static/js/tab.js
@@ -20,8 +20,42 @@
 
 $(document).ready(
   function(){
+    var showChar = 100;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Show more >";
+    var lesstext = "Show less";
+
+    $('.more').each(function() {
+      var content = $(this).html();
+
+      if(content.length > showChar) {
+        var c = content.substr(0, showChar);
+        var h = content.substr(showChar, content.length - showChar);
+
+        var html = c + '<span class="moreellipses">' + ellipsestext
+          + '&nbsp;</span><span class="morecontent"><span>' + h
+          + '</span>&nbsp;&nbsp;<a href="" class="morelink">'
+          + moretext + '</a></span>';
+
+        $(this).html(html);
+      }
+    });
+
+    $(".morelink").click(function(){
+      if($(this).hasClass("less")) {
+        $(this).removeClass("less");
+        $(this).html(moretext);
+      } else {
+        $(this).addClass("less");
+        $(this).html(lesstext);
+      }
+      $(this).parent().prev().toggle();
+      $(this).prev().toggle();
+      return false;
+    });
+
     var prefix = "tab_";
-	$('.tabbable .nav-pills a').click(function (e) {
+	  $('.tabbable .nav-pills a').click(function (e) {
         e.preventDefault();
         location.hash = $(e.target).attr('href').substr(1).replace(prefix, "");
         $(this).tab('show');
