0c2e4a175a135baf56df48dea0dc4de3ece9a35e
zhangduo <zhangduo@apache.org>
2016-08-04 09:36:39 +0800

    Add disk.sh
    Summary: check javac version before dist to ensure we use the right hadoop-security
    
    Test Plan: just run it
    
    Reviewers: yangzhe5, huzheng, zhangchen5, zhangguanghao1
    
    Reviewed By: zhangguanghao1
    
    Subscribers: infra-dev
    
    Differential Revision: https://phabricator.d.xiaomi.net/D37882

---
+++ dist.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+JAVA_VERSION=`java -version 2>&1 | grep version | cut -d\" -f2`
+echo $JAVA_VERSION | grep -qE "^1.8"
+if [ "$?" != "0" ]; then
+  echo "wrong java version $JAVA_VERSION, expected 1.6"
+  exit 1
+fi
+mvn -DskipTests clean install && mvn -DskipTests package assembly:single
