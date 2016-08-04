745d9557270ea1f20a30e2722a949a34264c255e
zhangduo <zhangduo@apache.org>
2016-08-04 13:58:00 +0800

    typo
    Summary: fix a typo
    
    Test Plan: N/A
    
    Reviewers: yangzhe5, huzheng, zhangchen5, zhangguanghao1
    
    Reviewed By: zhangguanghao1
    
    Subscribers: infra-dev
    
    Differential Revision: https://phabricator.d.xiaomi.net/D37919

--- dist.sh
+++ dist.sh
@@ -2,7 +2,7 @@
 JAVA_VERSION=`java -version 2>&1 | grep version | cut -d\" -f2`
 echo $JAVA_VERSION | grep -qE "^1.8"
 if [ "$?" != "0" ]; then
-  echo "wrong java version $JAVA_VERSION, expected 1.6"
+  echo "wrong java version $JAVA_VERSION, expected 1.8"
   exit 1
 fi
 mvn -DskipTests clean install && mvn -DskipTests package assembly:single
