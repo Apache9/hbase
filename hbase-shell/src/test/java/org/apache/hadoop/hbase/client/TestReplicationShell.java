cff24b91dca173a517e1d677ec1a1569ced5ce50
zhangguanghao1 <zhangguanghao1@xiaomi.com>
2017-06-06 17:16:32 +0800

    backport HBASE-13084 addendum to seperate TestReplicationShell from TestShell
    Summary: backport HBASE-13084 addendum to seperate TestReplicationShell from TestShell
    
    Test Plan: N/A
    
    Reviewers: huzheng, yangzhe5, tianjingyun, zhangduo
    
    Reviewed By: zhangduo
    
    Differential Revision: https://phabricator.d.xiaomi.net/D58332

--- hbase-shell/src/test/java/org/apache/hadoop/hbase/client/TestReplicationShell.java
+++ hbase-shell/src/test/java/org/apache/hadoop/hbase/client/TestReplicationShell.java
@@ -1,5 +1,4 @@
 /**
- *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -16,34 +15,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.hadoop.hbase.client;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.apache.hadoop.hbase.HBaseTestingUtility;
-import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.testclassification.LargeTests;
-import org.apache.hadoop.hbase.coprocessor.CoprocessorHost;
-import org.apache.hadoop.hbase.security.access.SecureTestUtil;
-import org.apache.hadoop.hbase.security.visibility.VisibilityTestUtil;
 import org.jruby.embed.PathType;
-import org.jruby.embed.ScriptingContainer;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 @Category(LargeTests.class)
-public class TestShell extends AbstractTestShell {
+public class TestReplicationShell extends AbstractTestShell {
 
   @Test
   public void testRunShellTests() throws IOException {
-    System.setProperty("shell.test.exclude", "replication_admin_test.rb");
+    System.setProperty("shell.test.include", "replication_admin_test.rb");
     // Start all ruby tests
     jruby.runScriptlet(PathType.ABSOLUTE, "src/test/ruby/tests_runner.rb");
   }
