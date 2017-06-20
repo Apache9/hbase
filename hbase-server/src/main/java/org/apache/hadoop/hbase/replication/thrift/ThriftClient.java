757e9b46febab8a404717f932b2b92ab3fbe033b
zhangduo <zhangduo@apache.org>
2017-06-20 09:46:20 +0800

    Add relogin logic for thrift replication client
    Summary: relogin when GSS init failed
    
    Test Plan: N/A
    
    Reviewers: yangzhe5, huzheng, tianjingyun, zhangguanghao1
    
    Reviewed By: zhangguanghao1
    
    Subscribers: hbase-help
    
    Differential Revision: https://phabricator.d.xiaomi.net/D59425

--- hbase-server/src/main/java/org/apache/hadoop/hbase/replication/thrift/ThriftClient.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/replication/thrift/ThriftClient.java
@@ -19,6 +19,17 @@
  */
 package org.apache.hadoop.hbase.replication.thrift;
 
+import java.io.IOException;
+import java.net.ConnectException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import javax.security.sasl.Sasl;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
@@ -27,8 +38,6 @@ import org.apache.hadoop.hbase.ServerName;
 import org.apache.hadoop.hbase.regionserver.wal.HLog;
 import org.apache.hadoop.hbase.replication.thrift.generated.TBatchEdit;
 import org.apache.hadoop.hbase.replication.thrift.generated.TEdit;
-import org.apache.hadoop.hbase.replication.thrift.generated.THBaseService;
-import org.apache.hadoop.hbase.replication.thrift.generated.TIOError;
 import org.apache.hadoop.hbase.replication.thrift.generated.THBaseService;
 import org.apache.hadoop.hbase.security.User;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -43,11 +52,6 @@ import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
 
-import javax.security.sasl.Sasl;
-import java.io.IOException;
-import java.util.*;
-import java.util.concurrent.ConcurrentSkipListMap;
-
 public class ThriftClient {
   private static final Log LOG = LogFactory.getLog(ThriftClient.class);
   // 0.98 support namespace to manager tables of the same business. We may need to
@@ -55,15 +59,15 @@ public class ThriftClient {
   // may be businessName_tablename, it could be transfered to businessName:tablename.
   // This option allow us to define the table name mapping for replication. The format
   // of the value should be 'sourceTable1=>destTable1,sourceTable2=>destTable2'
-  public static final String HBASE_REPLICATION_THRIFT_TABLE_NAME_MAP = "hbase.replication.thrift.tablename.map";
+  public static final String HBASE_REPLICATION_THRIFT_TABLE_NAME_MAP =
+      "hbase.replication.thrift.tablename.map";
   private static Object tableNameMapLock = new Object();
   public static Map<String, String> tableNameMap = null;
   private Configuration conf;
   private final String peerId;
   private boolean isSecure;
-  private ConcurrentSkipListMap<String, THBaseService.Client> clients =
-      new ConcurrentSkipListMap<String, THBaseService.Client>();
-
+  private ConcurrentMap<String, THBaseService.Client> clients =
+      new ConcurrentHashMap<String, THBaseService.Client>();
 
   public ThriftClient(Configuration conf, String peerId) throws IOException {
     this.conf = conf;
@@ -76,15 +80,16 @@ public class ThriftClient {
       return;
     }
     if (tableNameMap == null) {
-      synchronized(tableNameMapLock) {
+      synchronized (tableNameMapLock) {
         if (tableNameMap == null) {
           tableNameMap = new HashMap<String, String>();
-          //The format of the value should be 'sourceTable1=>destTable1,sourceTable2=>destTable2'
+          // The format of the value should be 'sourceTable1=>destTable1,sourceTable2=>destTable2'
           String[] mappingItems = mappingString.split(",");
           for (String mappingItem : mappingItems) {
             String[] names = mappingItem.split("=>");
             if (names.length != 2) {
-              throw new IOException("table name mapping string is error formatted, mappingString=" + mappingItem);
+              throw new IOException(
+                  "table name mapping string is error formatted, mappingString=" + mappingItem);
             }
             tableNameMap.put(names[0], names[1]);
           }
@@ -92,47 +97,64 @@ public class ThriftClient {
       }
     }
   }
-  
-  private THBaseService.Client createClient(String host, int port) throws IOException,
-      TTransportException {
-    boolean isCompact =
-        conf.getBoolean("hbase.replication.thrift.compact", true);
-    int timeout = conf.getInt(HConstants.HBASE_RPC_TIMEOUT_KEY,
-        HConstants.DEFAULT_HBASE_RPC_TIMEOUT);
+
+  private void safeClose(THBaseService.Client client) {
+    try {
+      client.getOutputProtocol().getTransport().close();
+    } catch (Exception e) {
+      LOG.warn("Failed to gracefully close broken transport.", e);
+    }
+  }
+
+  private THBaseService.Client createClient(String host, int port) throws IOException {
+    boolean isCompact = conf.getBoolean("hbase.replication.thrift.compact", true);
+    int timeout =
+        conf.getInt(HConstants.HBASE_RPC_TIMEOUT_KEY, HConstants.DEFAULT_HBASE_RPC_TIMEOUT);
     String serverProtocol = UserGroupInformation.getCurrentUser().getUserName();
     String serverAddress = null;
-    if(isSecure) {
+    if (isSecure) {
       String kerberosName = UserGroupInformation.getCurrentUser().getUserName();
       final String names[] = SaslRpcServer.splitKerberosName(kerberosName);
       if (names.length != 3) {
-        throw new TTransportException("Kerberos principal should have 3 parts: " + kerberosName);
+        throw new IOException("Kerberos principal should have 3 parts: " + kerberosName);
       }
       serverProtocol = names[0];
       serverAddress = names[1];
     }
 
     TTransport transport = new TSocket(host, port, timeout);
-    if(isSecure) {
+    if (isSecure) {
       Map<String, String> saslProps = new HashMap<String, String>();
       saslProps.put(Sasl.QOP, ThriftUtilities.getQOP(conf).getSaslQop());
       transport = new TUGIAssumingTransport(
-          new TSaslClientTransport(
-              ThriftUtilities.getAuthType(conf).getMechanismName(),
-              null,
-              serverProtocol, serverAddress,
-              saslProps, null,
-              transport),
+          new TSaslClientTransport(ThriftUtilities.getAuthType(conf).getMechanismName(), null,
+              serverProtocol, serverAddress, saslProps, null, transport),
           User.getCurrent().getUGI());
     }
     try {
       transport.open();
-      LOG.debug("Connected to "+host+":"+port);
+      LOG.debug("Connected to " + host + ":" + port);
     } catch (TTransportException e) {
-      throw new IOException("Failed to open transport connection to : "+host+":"+port + ", isSecure: " + isSecure, e);
+      if (e.getMessage().contains("GSS initiate failed")) {
+        if (UserGroupInformation.isLoginKerberosKeyBased()) {
+          UserGroupInformation.getLoginUser().reloginFromKerberosKey();
+        } else {
+          UserGroupInformation.getLoginUser().reloginFromTicketCache();
+        }
+      }
+
+      if (e.getCause() != null && e.getCause() instanceof ConnectException) {
+        ConnectException ce = new ConnectException("Failed to open transport connection to : " +
+            host + ":" + port + ", isSecure: " + isSecure);
+        ce.initCause(e);
+        throw ce;
+      }
+      throw new IOException("Failed to open transport connection to : " + host + ":" + port +
+          ", isSecure: " + isSecure);
     }
 
     TProtocol protocol;
-    if(isCompact) {
+    if (isCompact) {
       protocol = new TCompactProtocol(transport);
     } else {
       protocol = new TBinaryProtocol(transport);
@@ -141,21 +163,28 @@ public class ThriftClient {
     return new THBaseService.Client(protocol);
   }
 
-  public THBaseService.Client getClient(String host, int port) throws IOException, TTransportException {
-    String key = host+":"+port;
-    if(clients.containsKey(key)) {
-      return clients.get(key);
+  public THBaseService.Client getClient(String host, int port)
+      throws IOException, TTransportException {
+    String key = host + ":" + port;
+    THBaseService.Client client = clients.get(key);
+    if (client != null) {
+      return client;
     }
-    THBaseService.Client client = createClient(host, port);
-    clients.put(key, client);
-    return clients.get(key);
+    client = createClient(host, port);
+    THBaseService.Client existsClient = clients.putIfAbsent(key, client);
+    if (existsClient != null) {
+      safeClose(client);
+      return existsClient;
+    } else {
+      return client;
+    }
   }
 
   public void removeClient(String host, int port) {
-    clients.remove(host+":"+port);
+    clients.remove(host + ":" + port);
   }
 
-  public void ping(ServerName serverName) throws IOException{
+  public void ping(ServerName serverName) throws IOException {
     THBaseService.Client client = null;
     try {
       client = getClientFromServerName(serverName);
@@ -163,11 +192,7 @@ public class ThriftClient {
     } catch (TException e) {
       removeClient(serverName.getHostname(),
         ThriftUtilities.getDestinationPeerPort(conf, peerId, serverName.getPort()));
-      try {
-        client.getOutputProtocol().getTransport().close();
-      } catch(Exception e2) {
-        LOG.debug("Failed to gracefully close broken transport.", e2);
-      }
+      safeClose(client);
       throw new IOException("Failed to ping replication client", e);
     }
 
@@ -186,7 +211,7 @@ public class ThriftClient {
       }
     }
   }
-  
+
   public void shipEdits(ServerName serverName, List<HLog.Entry> entries) throws IOException {
     THBaseService.Client client;
     String host = serverName.getHostname();
@@ -205,11 +230,7 @@ public class ThriftClient {
       client.replicate(batchEdit);
     } catch (TTransportException e) {
       removeClient(host, port);
-      try {
-        client.getOutputProtocol().getTransport().close();
-      } catch(Exception e2) {
-        LOG.debug("Failed to gracefully close broken transport.", e2);
-      }
+      safeClose(client);
       throw new IOException("Failed to ship edits", e);
     } catch (TException e) {
       throw new IOException("Failed to ship edits", e);
@@ -228,7 +249,6 @@ public class ThriftClient {
       return null;
     }
   }
-
 
   private THBaseService.Client getClientFromServerName(ServerName serverName) throws IOException {
     THBaseService.Client client;
