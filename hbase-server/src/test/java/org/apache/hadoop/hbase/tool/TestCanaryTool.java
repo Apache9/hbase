04aa20a6066444c640857052f7067621056e1e6c
zhangduo <zhangduo@apache.org>
2017-06-21 18:26:34 +0800

    Use CountDownLatch to avoid buffer all the CompletableFutures
    Summary: avoid OOM
    
    Test Plan: UT
    
    Reviewers: zhangguanghao1, huzheng, tianjingyun, yangzhe5
    
    Reviewed By: yangzhe5
    
    Subscribers: hbase-help
    
    Differential Revision: https://phabricator.d.xiaomi.net/D59633

--- hbase-server/src/test/java/org/apache/hadoop/hbase/tool/TestCanaryTool.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/tool/TestCanaryTool.java
@@ -22,7 +22,7 @@ package org.apache.hadoop.hbase.tool;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.anyLong;
 import static org.mockito.Matchers.isA;
-import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
@@ -30,8 +30,11 @@ import static org.mockito.Mockito.verify;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HRegionInfo;
+import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.HConnection;
+import org.apache.hadoop.hbase.client.HConnectionManager;
+import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.testclassification.MediumTests;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -49,6 +52,7 @@ public class TestCanaryTool {
   private HBaseTestingUtility testingUtility;
   private static final byte[] FAMILY = Bytes.toBytes("f");
   private static final byte[] COLUMN = Bytes.toBytes("col");
+  private static byte[][] SPLIT_KEYS;
 
   @Rule
   public TestName name = new TestName();
@@ -57,6 +61,23 @@ public class TestCanaryTool {
   public void setUp() throws Exception {
     testingUtility = new HBaseTestingUtility();
     testingUtility.startMiniCluster();
+    SPLIT_KEYS = new byte[99][];
+    for (int i = 1; i < 100; i++) {
+      SPLIT_KEYS[i - 1] = Bytes.toBytes(String.format("%02d", i));
+    }
+    HTableDescriptor htd = new HTableDescriptor(TableName.valueOf(name.getMethodName()));
+    htd.addFamily(new HColumnDescriptor(FAMILY));
+    testingUtility.getHBaseAdmin().createTable(htd, SPLIT_KEYS);
+    try (HConnection conn = HConnectionManager.createConnection(testingUtility.getConfiguration());
+        HTableInterface table = conn.getTable(htd.getTableName())) {
+      for (int i = 0; i < 1000; i++) {
+        byte[] row = Bytes.toBytes(String.format("%03d", i));
+        Put p = new Put(row);
+        p.add(FAMILY, COLUMN, row);
+        table.put(p);
+      }
+    }
+    testingUtility.getConfiguration().setInt("hbase.canary.concurrency.max", 10);
   }
 
   @After
@@ -66,20 +87,11 @@ public class TestCanaryTool {
 
   @Test
   public void testBasicCanaryWorks() throws Exception {
-    final TableName tableName = TableName.valueOf(name.getMethodName());
-    HTable table = testingUtility.createTable(tableName, new byte[][] { FAMILY });
-    // insert some test rows
-    for (int i = 0; i < 1000; i++) {
-      byte[] iBytes = Bytes.toBytes(i);
-      Put p = new Put(iBytes);
-      p.add(FAMILY, COLUMN, iBytes);
-      table.put(p);
-    }
     Canary.StdOutSink sink = spy(new Canary.StdOutSink());
     Canary canary = new Canary(sink);
     String[] args = { name.getMethodName() };
     assertEquals(0, ToolRunner.run(testingUtility.getConfiguration(), canary, args));
-    verify(sink, atLeastOnce()).publishReadTiming(isA(HRegionInfo.class),
+    verify(sink, atLeast(SPLIT_KEYS.length + 1)).publishReadTiming(isA(HRegionInfo.class),
       isA(HColumnDescriptor.class), anyLong());
     verify(sink, never()).publishReadFailure(isA(HRegionInfo.class), isA(Throwable.class));
     verify(sink, never()).publishReadFailure(isA(HRegionInfo.class), isA(HColumnDescriptor.class),
