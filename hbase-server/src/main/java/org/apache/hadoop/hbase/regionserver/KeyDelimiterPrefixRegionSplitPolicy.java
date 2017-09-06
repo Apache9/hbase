58507a6e0b09c63190d6ce34301ddf9533d5afff
zhangduo <zhangduo@apache.org>
2017-09-06 23:03:27 +0800

    Add a constant size key prefix region split policy
    Summary: Avoid splitting too frequently
    
    Test Plan: N/A
    
    Reviewers: zhangguanghao1, huzheng, tianjingyun, wuguoquan, meiyi
    
    Reviewed By: huzheng
    
    Subscribers: hbase-help
    
    Differential Revision: https://phabricator.d.xiaomi.net/D67677

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/KeyDelimiterPrefixRegionSplitPolicy.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/KeyDelimiterPrefixRegionSplitPolicy.java
@@ -1,9 +1,27 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hbase.regionserver;
 
 import java.util.Arrays;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hbase.classification.InterfaceAudience;
 import org.apache.hadoop.hbase.util.Bytes;
 
 /**
@@ -13,7 +31,7 @@ import org.apache.hadoop.hbase.util.Bytes;
  * This ensures that a region is not split "inside" row keys with same first item.
  * I.e. rows can be co-located in a region by first item of a row key
  */
-
+@InterfaceAudience.Private
 public class KeyDelimiterPrefixRegionSplitPolicy extends ConstantSizeRegionSplitPolicy {
   static final Log LOG = LogFactory
       .getLog(KeyDelimiterPrefixRegionSplitPolicy.class);
