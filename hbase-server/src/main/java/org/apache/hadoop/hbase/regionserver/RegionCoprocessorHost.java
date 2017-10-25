8d92cd0057e075708e956fee23f378cf7de550e5
zhangduo <zhangduo@apache.org>
2017-10-25 11:00:44 +0800

    HBASE-18905 Allow CPs to request flush on Region and know the completion of the requested flush

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RegionCoprocessorHost.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/RegionCoprocessorHost.java
@@ -663,13 +663,13 @@ public class RegionCoprocessorHost
    * Invoked before a memstore flush
    * @throws IOException
    */
-  public InternalScanner preFlush(HStore store, final InternalScanner scanner)
-      throws IOException {
-    return execOperationWithResult(false, scanner, coprocEnvironments.isEmpty() ? null :
-        new ObserverOperationWithResult<RegionObserver, InternalScanner>(regionObserverGetter) {
+  public InternalScanner preFlush(HStore store, InternalScanner scanner,
+      FlushLifeCycleTracker tracker) throws IOException {
+    return execOperationWithResult(false, scanner, coprocEnvironments.isEmpty() ? null
+        : new ObserverOperationWithResult<RegionObserver, InternalScanner>(regionObserverGetter) {
           @Override
           public InternalScanner call(RegionObserver observer) throws IOException {
-            return observer.preFlush(this, store, getResult());
+            return observer.preFlush(this, store, getResult(), tracker);
           }
         });
   }
@@ -678,11 +678,11 @@ public class RegionCoprocessorHost
    * Invoked before a memstore flush
    * @throws IOException
    */
-  public void preFlush() throws IOException {
+  public void preFlush(FlushLifeCycleTracker tracker) throws IOException {
     execOperation(coprocEnvironments.isEmpty() ? null : new RegionObserverOperation() {
       @Override
       public void call(RegionObserver observer) throws IOException {
-        observer.preFlush(this);
+        observer.preFlush(this, tracker);
       }
     });
   }
@@ -691,11 +691,11 @@ public class RegionCoprocessorHost
    * Invoked after a memstore flush
    * @throws IOException
    */
-  public void postFlush() throws IOException {
+  public void postFlush(FlushLifeCycleTracker tracker) throws IOException {
     execOperation(coprocEnvironments.isEmpty() ? null : new RegionObserverOperation() {
       @Override
       public void call(RegionObserver observer) throws IOException {
-        observer.postFlush(this);
+        observer.postFlush(this, tracker);
       }
     });
   }
@@ -704,11 +704,12 @@ public class RegionCoprocessorHost
    * Invoked after a memstore flush
    * @throws IOException
    */
-  public void postFlush(final HStore store, final HStoreFile storeFile) throws IOException {
+  public void postFlush(HStore store, HStoreFile storeFile, FlushLifeCycleTracker tracker)
+      throws IOException {
     execOperation(coprocEnvironments.isEmpty() ? null : new RegionObserverOperation() {
       @Override
       public void call(RegionObserver observer) throws IOException {
-        observer.postFlush(this, store, storeFile);
+        observer.postFlush(this, store, storeFile, tracker);
       }
     });
   }
