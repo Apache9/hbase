b8c69e008af4df8258faaf6a61a0ef2dfb53ecba
zhangduo <zhangduo@apache.org>
2017-12-20 18:21:00 +0800

    HBASE-19564 Procedure id is missing in the response of peer related operations

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/replication/ReplicationManager.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/replication/ReplicationManager.java
@@ -131,9 +131,9 @@ public class ReplicationManager {
       checkNamespacesAndTableCfsConfigConflict(peerConfig.getExcludeNamespaces(),
         peerConfig.getExcludeTableCFsMap());
     } else {
-      if ((peerConfig.getExcludeNamespaces() != null && !peerConfig.getNamespaces().isEmpty())
+      if ((peerConfig.getExcludeNamespaces() != null && !peerConfig.getExcludeNamespaces().isEmpty())
           || (peerConfig.getExcludeTableCFsMap() != null
-              && !peerConfig.getTableCFsMap().isEmpty())) {
+              && !peerConfig.getExcludeTableCFsMap().isEmpty())) {
         throw new IllegalArgumentException(
             "Need clean exclude-namespaces or exclude-table-cfs config firstly"
                 + " when replicate_all flag is false");
