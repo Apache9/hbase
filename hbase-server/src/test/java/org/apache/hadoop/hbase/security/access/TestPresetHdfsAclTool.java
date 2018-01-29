ef568b53c1cca90a009c5430834e21111777e75a
meiyi <meiyi@xiaomi.com>
2018-01-29 17:18:35 +0800

    add count user num tool
    Reviewers: zhangduo, zhangguanghao1, huzheng, tianjingyun, wuguoquan
    
    Reviewed By: huzheng
    
    Maniphest Tasks: T10707
    
    Differential Revision: https://phabricator.d.xiaomi.net/D83083

--- hbase-server/src/test/java/org/apache/hadoop/hbase/security/access/TestPresetHdfsAclTool.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/security/access/TestPresetHdfsAclTool.java
@@ -18,6 +18,9 @@
 
 package org.apache.hadoop.hbase.security.access;
 
+import static org.apache.hadoop.hbase.security.access.Permission.Action.READ;
+import static org.apache.hadoop.hbase.security.access.Permission.Action.WRITE;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
@@ -30,7 +33,6 @@ import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.security.User;
-import org.apache.hadoop.hbase.security.access.Permission.Action;
 import org.apache.hadoop.hbase.testclassification.SmallTests;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.AfterClass;
@@ -39,9 +41,12 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import java.io.IOException;
+import java.util.Map;
+
 @Category({ SmallTests.class })
 public class TestPresetHdfsAclTool {
-  private static Log LOG = LogFactory.getLog(TestHdfsAclManager.class);
+  private static Log LOG = LogFactory.getLog(TestPresetHdfsAclTool.class);
 
   private static HBaseTestingUtility TEST_UTIL = new HBaseTestingUtility();
   private static Configuration conf = TEST_UTIL.getConfiguration();
@@ -87,17 +92,18 @@ public class TestPresetHdfsAclTool {
     String snapshot = "testGrantNamespace" + SNAPSHOT_POSTFIX;
     String snapshot2 = "testGrantNamespace2" + SNAPSHOT_POSTFIX;
 
-    TestHdfsAclManager.createNamespace(admin, namespace);
-    HTable hTable = TestHdfsAclManager.createTable(TEST_UTIL, table);
-    TestHdfsAclManager.put(hTable);
+    createNamespace(namespace);
+    HTable hTable = createTable(table);
+    put(hTable);
     admin.snapshot(snapshot, table);
-    SecureTestUtil.grantOnNamespace(TEST_UTIL, GRANT_USER, namespace, Permission.Action.READ);
+    SecureTestUtil.grantOnNamespace(TEST_UTIL, GRANT_USER, namespace, READ);
     admin.snapshot(snapshot2, table);
+    hTable.close();
 
     presetHdfsAcl();
 
-    Assert.assertTrue(TestHdfsAclManager.canUserScanSnapshot(conf, fs, grantUser, snapshot));
-    Assert.assertTrue(TestHdfsAclManager.canUserScanSnapshot(conf, fs, grantUser, snapshot2));
+    Assert.assertTrue(canUserScanSnapshot(grantUser, snapshot));
+    Assert.assertTrue(canUserScanSnapshot(grantUser, snapshot2));
   }
 
   @Test
@@ -107,19 +113,19 @@ public class TestPresetHdfsAclTool {
     String snapshot = "testGrantTable" + SNAPSHOT_POSTFIX;
     String snapshot2 = "testGrantTable2" + SNAPSHOT_POSTFIX;
 
-    TestHdfsAclManager.createNamespace(admin, namespace);
-    HTable hTable = TestHdfsAclManager.createTable(TEST_UTIL, table);
-    TestHdfsAclManager.put(hTable);
+    createNamespace(namespace);
+    HTable hTable = createTable(table);
+    put(hTable);
     admin.snapshot(snapshot, table);
-    SecureTestUtil.grantOnTable(TEST_UTIL, GRANT_USER, TableName.valueOf(table), null, null,
-      Permission.Action.READ);
-    TestHdfsAclManager.put2(hTable);
+    grantOnTable(GRANT_USER, table, READ);
+    put2(hTable);
     admin.snapshot(snapshot2, table);
+    hTable.close();
 
     presetHdfsAcl();
 
-    Assert.assertTrue(TestHdfsAclManager.canUserScanSnapshot(conf, fs, grantUser, snapshot));
-    Assert.assertTrue(TestHdfsAclManager.canUserScanSnapshot(conf, fs, grantUser, snapshot2));
+    Assert.assertTrue(canUserScanSnapshot(grantUser, snapshot));
+    Assert.assertTrue(canUserScanSnapshot(grantUser, snapshot2));
   }
 
   @Test
@@ -128,21 +134,21 @@ public class TestPresetHdfsAclTool {
     String snapshot = "testCompactTable" + SNAPSHOT_POSTFIX;
     String snapshot2 = "testCompactTable2" + SNAPSHOT_POSTFIX;
 
-    HTable htable = TestHdfsAclManager.createTable(TEST_UTIL, table);
-    TestHdfsAclManager.put(htable);
+    HTable htable = createTable(table);
+    put(htable);
     admin.snapshot(snapshot, table);
-    SecureTestUtil.grantOnTable(TEST_UTIL, GRANT_USER, TableName.valueOf(table), null, null,
-      Permission.Action.READ);
+    grantOnTable(GRANT_USER, table, READ);
 
-    TestHdfsAclManager.put2(htable);
+    put2(htable);
     admin.snapshot(snapshot2, table);
     admin.flush(table);
     admin.majorCompact(table);
+    htable.close();
 
     presetHdfsAcl();
 
-    Assert.assertTrue(TestHdfsAclManager.canUserScanSnapshot(conf, fs, grantUser, snapshot));
-    Assert.assertTrue(TestHdfsAclManager.canUserScanSnapshot(conf, fs, grantUser, snapshot2));
+    Assert.assertTrue(canUserScanSnapshot(grantUser, snapshot));
+    Assert.assertTrue(canUserScanSnapshot(grantUser, snapshot2));
   }
 
   @Test
@@ -151,53 +157,149 @@ public class TestPresetHdfsAclTool {
     String snapshot = "testSplitTable" + SNAPSHOT_POSTFIX;
     String snapshot2 = "testSplitTable2" + SNAPSHOT_POSTFIX;
 
-    HTable htable = TestHdfsAclManager.createTable(TEST_UTIL, table);
-    TestHdfsAclManager.put(htable);
+    HTable htable = createTable(table);
+    put(htable);
     admin.snapshot(snapshot, table);
-    SecureTestUtil.grantOnTable(TEST_UTIL, GRANT_USER, TableName.valueOf(table), null, null,
-      Permission.Action.READ);
+    grantOnTable(GRANT_USER, table, READ);
 
-    TestHdfsAclManager.put2(htable);
+    put2(htable);
     admin.snapshot(snapshot2, table);
     admin.split(table, "3");
     admin.split(table, "4");
     admin.flush(table);
     admin.majorCompact(table);
+    htable.close();
 
     presetHdfsAcl();
 
-    Assert.assertTrue(TestHdfsAclManager.canUserScanSnapshot(conf, fs, grantUser, snapshot));
-    Assert.assertTrue(TestHdfsAclManager.canUserScanSnapshot(conf, fs, grantUser, snapshot2));
+    Assert.assertTrue(canUserScanSnapshot(grantUser, snapshot));
+    Assert.assertTrue(canUserScanSnapshot(grantUser, snapshot2));
   }
 
   @Test
   public void testCheckDuplicateHBaseGrants() throws Exception {
     String ns = "ns_testCheckDuplicateHBaseGrants";
     TableName tbName = TableName.valueOf(ns, "testCheckDuplicateHBaseGrants");
-    TestHdfsAclManager.createNamespace(admin, ns);
-    try (HTable table = TestHdfsAclManager.createTable(TEST_UTIL, tbName.toString())) {
+    createNamespace(ns);
+    try (HTable table = createTable(tbName.toString())) {
       PresetHdfsAclTool tool = new PresetHdfsAclTool(conf);
       Assert.assertEquals(0, tool.checkDuplicateGrantInHBaseACL());
 
-      SecureTestUtil.grantOnNamespace(TEST_UTIL, GRANT_USER, ns, Permission.Action.READ,
-        Permission.Action.WRITE);
-      SecureTestUtil.grantOnTable(TEST_UTIL, GRANT_USER, tbName, null, null, Action.READ,
-        Action.WRITE);
+      SecureTestUtil.grantOnNamespace(TEST_UTIL, GRANT_USER, ns, READ, WRITE);
+      SecureTestUtil.grantOnTable(TEST_UTIL, GRANT_USER, tbName, null, null, READ, WRITE);
       Assert.assertEquals(1, tool.checkDuplicateGrantInHBaseACL());
 
-      SecureTestUtil.grantOnTable(TEST_UTIL, GRANT_USER, tbName, Bytes.toBytes("A"), null,
-        Action.WRITE);
+      SecureTestUtil.grantOnTable(TEST_UTIL, GRANT_USER, tbName, Bytes.toBytes("A"), null, WRITE);
       Assert.assertEquals(3, tool.checkDuplicateGrantInHBaseACL());
 
       SecureTestUtil.grantOnTable(TEST_UTIL, GRANT_USER, tbName, Bytes.toBytes("A"),
-        Bytes.toBytes("Q"), Action.WRITE);
+        Bytes.toBytes("Q"), WRITE);
       Assert.assertEquals(6, tool.checkDuplicateGrantInHBaseACL());
+
+      table.close();
     }
+  }
+
+  @Test
+  public void testCheckHdfsAclExceeded() throws Exception {
+    // 2 global
+    String colon = ":";
+    String defaultNs = "default"; // 1 -> 3
+    String ns1 = "ns1"; // 2 -> 4
+    String ns2 = "ns2"; // 0 -> 2
+    String t1 = "t1"; // 1 -> 4
+    String t2 = "t2"; // 0 -> 3
+    String ns1T1 = ns1 + colon + t1; // 2 -> 6
+    String ns1T2 = ns1 + colon + t2; // 1 -> 5
+    String ns2T1 = ns2 + colon + t1; // 1 -> 3
+
+    String globalUser1 = "h_g1";
+    String globalUser2 = "u_g2";
+    String globalUser3 = "hbase_tst_admin";
+    String nsUser1 = "h_n1";
+    String nsUser2 = "h_n2";
+    String nsUser3 = "h_n3";
+    String tableUser1 = "u_t1";
+    String tableUser2 = "h_t2";
+    String tableUser3 = "h_t3";
+
+    createNamespace(ns1);
+    createNamespace(ns2);
+    createTable(t1);
+    createTable(t2);
+    createTable(ns1T1);
+    createTable(ns1T2);
+    createTable(ns2T1);
+
+    SecureTestUtil.grantGlobal(TEST_UTIL, globalUser1, READ);
+    SecureTestUtil.grantGlobal(TEST_UTIL, globalUser2, READ);
+    SecureTestUtil.grantGlobal(TEST_UTIL, globalUser3, READ);
+    SecureTestUtil.grantOnNamespace(TEST_UTIL, nsUser3, defaultNs, READ);
+    SecureTestUtil.grantOnNamespace(TEST_UTIL, nsUser1, ns1, READ);
+    SecureTestUtil.grantOnNamespace(TEST_UTIL, nsUser2, ns1, READ);
+    grantOnTable(tableUser1, t1, READ);
+    grantOnTable(tableUser1, ns1T1, READ);
+    grantOnTable(tableUser2, ns1T1, READ);
+    grantOnTable(tableUser3, ns1T2, READ);
+    grantOnTable(nsUser1, ns1T2, READ);
+    grantOnTable(tableUser1, ns2T1, READ);
+    SecureTestUtil.grantOnTable(TEST_UTIL, GRANT_USER, TableName.valueOf(t1), Bytes.toBytes("A"), null, WRITE);
+
+    conf.setBoolean(HConstants.HDFS_ACL_ENABLE, true);
+    PresetHdfsAclTool tool = new PresetHdfsAclTool(conf);
+    Map<String, Integer> userNumMap = tool.checkHdfsAclEntryExceeded();
+    Assert.assertEquals(3, userNumMap.get(defaultNs).intValue());
+    Assert.assertEquals(4, userNumMap.get(ns1).intValue());
+    Assert.assertEquals(4, userNumMap.get(t1).intValue());
+    Assert.assertEquals(6, userNumMap.get(ns1T1).intValue());
+    Assert.assertEquals(5, userNumMap.get(ns1T2).intValue());
+    Assert.assertEquals(3, userNumMap.get(ns2T1).intValue());
+
+    SecureTestUtil.revokeGlobal(TEST_UTIL, globalUser1);
+    SecureTestUtil.revokeGlobal(TEST_UTIL, globalUser2);
+    SecureTestUtil.revokeGlobal(TEST_UTIL, globalUser3);
+    SecureTestUtil.revokeFromNamespace(TEST_UTIL, nsUser3, defaultNs, READ);
+    SecureTestUtil.revokeFromNamespace(TEST_UTIL, nsUser1, ns1, READ);
+    SecureTestUtil.revokeFromNamespace(TEST_UTIL, nsUser2, ns1, READ);
+    revokeFromTable(tableUser1, t1, READ);
+    revokeFromTable(tableUser1, ns1T1, READ);
+    revokeFromTable(tableUser2, ns1T1, READ);
+    revokeFromTable(tableUser3, ns1T2, READ);
+    revokeFromTable(nsUser1, ns1T2, READ);
+    revokeFromTable(tableUser1, ns2T1, READ);
   }
 
   private void presetHdfsAcl() {
     conf.setBoolean(HConstants.HDFS_ACL_ENABLE, true);
     PresetHdfsAclTool tool = new PresetHdfsAclTool(conf);
-    tool.execute();
+    tool.presetHdfsAcl();
+  }
+
+  private void revokeFromTable(String user, String table, Permission.Action... actions) throws Exception{
+    SecureTestUtil.revokeFromTable(TEST_UTIL, user, TableName.valueOf(table), null, null, actions);
+  }
+
+  private void grantOnTable(String user, String table, Permission.Action... actions) throws Exception{
+    SecureTestUtil.grantOnTable(TEST_UTIL, user, TableName.valueOf(table), null, null, actions);
+  }
+
+  private void createNamespace(String namespace) throws IOException{
+    TestHdfsAclManager.createNamespace(admin, namespace);
+  }
+
+  private HTable createTable(String table) throws IOException{
+    return TestHdfsAclManager.createTable(TEST_UTIL, conf, table);
+  }
+
+  private void put(HTable hTable) throws IOException{
+    TestHdfsAclManager.put(hTable);
+  }
+
+  private void put2(HTable hTable) throws IOException{
+    TestHdfsAclManager.put2(hTable);
+  }
+
+  private boolean canUserScanSnapshot(User user, String snapshot) throws IOException, InterruptedException{
+    return TestHdfsAclManager.canUserScanSnapshot(conf, fs, user, snapshot);
   }
 }
