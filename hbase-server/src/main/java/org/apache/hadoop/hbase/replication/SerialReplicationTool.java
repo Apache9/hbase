b59ae6703dd68d60c86e5f38002e9d2b19b7d2d7
Guanghao Zhang <zhangguanghao1@xiaomi.com>
2018-06-25 10:26:46 +0800

    Cleanup unused barriers when fix serial replication
    Summary: Cleanup unused barriers when fix serial replication
    
    Test Plan: N/A
    
    Reviewers: huzheng, tianjingyun, meiyi, haxiaolin, xujunhong, niuyulin, zhangduo
    
    Reviewed By: zhangduo
    
    Subscribers: sonarqube
    
    Maniphest Tasks: T11130
    
    Differential Revision: https://phabricator.d.xiaomi.net/D101142

--- hbase-server/src/main/java/org/apache/hadoop/hbase/replication/SerialReplicationTool.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/replication/SerialReplicationTool.java
@@ -19,7 +19,6 @@ package org.apache.hadoop.hbase.replication;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,10 +28,14 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.catalog.MetaEditor;
 import org.apache.hadoop.hbase.classification.InterfaceAudience;
+import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.HConnection;
 import org.apache.hadoop.hbase.client.HConnectionManager;
+import org.apache.hadoop.hbase.client.HTableInterface;
+import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -49,8 +52,11 @@ public class SerialReplicationTool extends Configured implements Tool {
 
   private HConnection connection;
 
+  private HTableInterface metaTable;
+
   public SerialReplicationTool(Configuration conf) throws IOException {
     connection = HConnectionManager.createConnection(conf);
+    metaTable = connection.getTable(TableName.META_TABLE_NAME);
   }
 
   /**
@@ -78,6 +84,24 @@ public class SerialReplicationTool extends Configured implements Tool {
     System.err.println();
   }
 
+  private void cleanUnusedBarriers(String encodedRegionName, List<Long> barriers) 
+      throws IOException {
+    Delete delete = new Delete(Bytes.toBytes(encodedRegionName));
+    for (int i = 0; i < barriers.size() - 1; i++) {
+      delete.deleteColumn(HConstants.REPLICATION_BARRIER_FAMILY,
+          Bytes.toBytes(barriers.get(i)));
+      LOG.info("Cleanup barrier " + barriers.get(i) + " for region=" + encodedRegionName);
+    }
+    metaTable.delete(delete);
+  }
+
+  private void updatePeerReplicationPosition(String peerId, String encodedRegionName, long pos)
+      throws IOException {
+    Map<String, Long> map = new HashMap<>();
+    map.put(encodedRegionName, pos);
+    MetaEditor.updateReplicationPositions(connection, peerId, map);
+  }
+
   private void fixStuckPeer(String peerId) throws Exception {
     Map<String, List<Long>> barrierMap = MetaEditor.getAllBarriers(connection);
     Map<String, Long> stuckRegions = getStuckRegions(barrierMap, peerId);
@@ -86,6 +110,7 @@ public class SerialReplicationTool extends Configured implements Tool {
     // Sleep again when try to fix
     LOG.info("Will sleep " + SLEEP_TIME + " ms to see whether position in meta was updated");
     Thread.sleep(SLEEP_TIME);
+
     for (Map.Entry<String, Long> entry : stuckRegions.entrySet()) {
       String encodedRegionName = entry.getKey();
       long posInMeta = MetaEditor.getReplicationPositionForOnePeer(connection,
@@ -99,22 +124,19 @@ public class SerialReplicationTool extends Configured implements Tool {
           // update new pos to last barrier - 1
           long newPos = barriers.get(barriers.size() - 1) - 1;
           if (posInMeta < newPos) {
-            Map<String, Long> map = new HashMap<>();
-            map.put(encodedRegionName, newPos);
-            MetaEditor.updateReplicationPositions(connection, peerId, map);
+            // cleanup unused barriers
+            if (barriers.size() - 1 > 0) {
+              cleanUnusedBarriers(encodedRegionName, barriers);
+            }
+            updatePeerReplicationPosition(peerId, encodedRegionName, newPos);
             LOG.info("Update region=" + encodedRegionName + " posInMeta from " + posInMeta + " to "
                 + newPos + ", barriers=" + Arrays.toString(barriers.toArray()) + ", peerId="
                 + peerId);
-          } else {
-            LOG.info(
-                "No need to fix for region=" + encodedRegionName + " because posInMeta=" + posInMeta
-                    + " is equal or bigger than newPos=" + newPos + ", barriers=" + Arrays
-                    .toString(barriers.toArray()) + ", peerId=" + peerId);
           }
-        } else {
-          LOG.info(
-            "No barriers found for region=" + encodedRegionName + ", posInMeta=" + posInMeta);
         }
+      } else {
+        LOG.info("No need to fix for region=" + encodedRegionName + " because posInMeta was updated from "
+            + entry.getValue() + " to " + posInMeta);
       }
     }
   }
@@ -139,15 +161,32 @@ public class SerialReplicationTool extends Configured implements Tool {
     // Sleep to see whether position in meta was updated
     LOG.info("Will sleep " + SLEEP_TIME + " ms to see whether position in meta was updated");
     Thread.sleep(SLEEP_TIME);
+
     Map<String, Long> stuckRegions = new HashMap<>();
     for (Map.Entry<String, List<Long>> entry : barrierMap.entrySet()) {
       String encodedRegionName = entry.getKey();
       long posInMeta = MetaEditor.getReplicationPositionForOnePeer(connection,
         Bytes.toBytes(encodedRegionName), peerId);
       if (posInMeta == posInMetaMap.get(encodedRegionName).longValue()) {
-        stuckRegions.put(encodedRegionName, posInMeta);
-        LOG.info("Peer " + peerId + " maybe stuck by region=" + encodedRegionName + ", posInMeta="
-            + posInMeta + ", barriers=" + Arrays.toString(entry.getValue().toArray()));
+        List<Long> barriers = entry.getValue();
+        if (barriers.size() > 0) {
+          long newPos = barriers.get(barriers.size() - 1) - 1;
+          if (posInMeta < newPos) {
+            stuckRegions.put(encodedRegionName, posInMeta);
+            LOG.info("Peer " + peerId + " maybe stuck by region=" + encodedRegionName + ", posInMeta="
+                + posInMeta + ", barriers=" + Arrays.toString(entry.getValue().toArray()));
+          } else {
+            LOG.info("Not stuck by region=" + encodedRegionName + " because posInMeta=" + posInMeta
+                + " is equal or bigger than newPos=" + newPos + ", barriers=" + Arrays
+                .toString(barriers.toArray()) + ", peerId=" + peerId);
+          }
+        } else {
+          LOG.info("Not stuck by region=" + encodedRegionName + ", posInMeta=" + posInMeta
+              + ", because no barriers");
+        }
+      } else {
+        LOG.info("Not stuck by region=" + encodedRegionName + " because posInMeta was updated from "
+            + posInMetaMap.get(encodedRegionName) + " to " + posInMeta);
       }
     }
     return stuckRegions;
@@ -177,6 +216,8 @@ public class SerialReplicationTool extends Configured implements Tool {
       printUsage();
       return 1;
     }
+    metaTable.close();
+    connection.close();
     return 0;
   }
 }
