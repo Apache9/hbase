f690badfa1b1836f9acdb90609ad4b21e04f811a
Guanghao Zhang <zhangguanghao1@xiaomi.com>
2018-08-29 15:18:27 +0800

    TableRecordReaderImpl should handle cursor result too
    Summary: TableRecordReaderImpl should handle cursor result too
    
    Test Plan: N/A
    
    Reviewers: huzheng, tianjingyun, meiyi, haxiaolin, linshangquan, zhangduo
    
    Reviewed By: zhangduo
    
    Differential Revision: https://phabricator.d.xiaomi.net/D112987

--- hbase-server/src/main/java/org/apache/hadoop/hbase/mapreduce/TableRecordReaderImpl.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/mapreduce/TableRecordReaderImpl.java
@@ -239,8 +239,16 @@ public class TableRecordReaderImpl {
         value = scanner.next();
         numRestarts++;
       }
+
       if (value != null && value.size() > 0) {
         key.set(value.getRow());
+        lastSuccessfulRow = key.get();
+        return true;
+      }
+
+      // Need handle cursor result
+      if (value != null && value.isCursor()) {
+        key.set(value.getCursor().getRow());
         lastSuccessfulRow = key.get();
         return true;
       }
