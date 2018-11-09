1b920639ac2c6913d8d83135ec2029b60d699373
huzheng <huzheng@xiaomi.com>
2018-11-09 14:43:30 +0800

    mapred.TableOutputFormat support the salted table
    Reviewers: zhangguanghao1, zhangduo, tianjingyun, meiyi, haxiaolin, xujunhong
    
    Reviewed By: zhangduo
    
    Differential Revision: https://phabricator.d.xiaomi.net/D124517

--- hbase-server/src/main/java/org/apache/hadoop/hbase/mapred/TableOutputFormat.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/mapred/TableOutputFormat.java
@@ -27,15 +27,16 @@ import org.apache.hadoop.hbase.classification.InterfaceStability;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
-import org.apache.hadoop.fs.FileAlreadyExistsException;
-import org.apache.hadoop.mapred.InvalidJobConfException;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.RecordWriter;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.util.Progressable;
+
+import com.xiaomi.infra.hbase.salted.SaltedHTable;
 
 /**
  * Convert Map/Reduce output and write it to an HBase table
@@ -43,53 +44,49 @@ import org.apache.hadoop.util.Progressable;
 @Deprecated
 @InterfaceAudience.Public
 @InterfaceStability.Stable
-public class TableOutputFormat extends
-FileOutputFormat<ImmutableBytesWritable, Put> {
+public class TableOutputFormat extends FileOutputFormat<ImmutableBytesWritable, Put> {
 
   /** JobConf parameter that specifies the output table */
   public static final String OUTPUT_TABLE = "hbase.mapred.outputtable";
   private final Log LOG = LogFactory.getLog(TableOutputFormat.class);
 
   /**
-   * Convert Reduce output (key, value) to (HStoreKey, KeyedDataArrayWritable)
-   * and write to an HBase table
+   * Convert Reduce output (key, value) to (HStoreKey, KeyedDataArrayWritable) and write to an HBase
+   * table
    */
-  protected static class TableRecordWriter
-    implements RecordWriter<ImmutableBytesWritable, Put> {
-    private HTable m_table;
+  protected static class TableRecordWriter implements RecordWriter<ImmutableBytesWritable, Put> {
+    private HTableInterface m_table;
 
     /**
      * Instantiate a TableRecordWriter with the HBase HClient for writing.
-     *
      * @param table
      */
-    public TableRecordWriter(HTable table) {
+    public TableRecordWriter(HTableInterface table) {
       m_table = table;
     }
 
-    public void close(Reporter reporter)
-      throws IOException {
+    public void close(Reporter reporter) throws IOException {
       m_table.close();
     }
 
-    public void write(ImmutableBytesWritable key,
-        Put value) throws IOException {
+    public void write(ImmutableBytesWritable key, Put value) throws IOException {
       m_table.put(new Put(value));
     }
   }
 
   @Override
   @SuppressWarnings("unchecked")
-  public RecordWriter getRecordWriter(FileSystem ignored,
-      JobConf job, String name, Progressable progress) throws IOException {
-
+  public RecordWriter getRecordWriter(FileSystem ignored, JobConf job, String name,
+      Progressable progress) throws IOException {
     // expecting exactly one path
-
     String tableName = job.get(OUTPUT_TABLE);
-    HTable table = null;
+    HTableInterface table;
     try {
       table = new HTable(HBaseConfiguration.create(job), tableName);
-    } catch(IOException e) {
+      if (table.getTableDescriptor().isSalted()) {
+        table = new SaltedHTable(table);
+      }
+    } catch (IOException e) {
       LOG.error(e);
       throw e;
     }
@@ -98,11 +95,9 @@ FileOutputFormat<ImmutableBytesWritable, Put> {
   }
 
   @Override
-  public void checkOutputSpecs(FileSystem ignored, JobConf job)
-  throws FileAlreadyExistsException, InvalidJobConfException, IOException {
-
+  public void checkOutputSpecs(FileSystem ignored, JobConf job) throws IOException {
     String tableName = job.get(OUTPUT_TABLE);
-    if(tableName == null) {
+    if (tableName == null) {
       throw new IOException("Must specify table name");
     }
   }
