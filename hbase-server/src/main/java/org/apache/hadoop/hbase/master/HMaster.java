130c5f026dc15f318733397f81f88890f6eea5bd
Viraj Jasani <virajjasani007@gmail.com>
2019-11-05 11:13:44 +0530

    HBASE-23212 Dynamically reload configs for Region Recovery chore (#802)
    Signed-off-by: Andrew Purtell <apurtell@apache.org>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/HMaster.java
@@ -431,6 +431,8 @@ public class HMaster extends HRegionServer implements MasterServices {
   private MasterProcedureManagerHost mpmHost;
 
   private RegionsRecoveryChore regionsRecoveryChore = null;
+
+  private RegionsRecoveryConfigManager regionsRecoveryConfigManager = null;
   // it is assigned after 'initialized' guard set to true, so should be volatile
   private volatile MasterQuotaManager quotaManager;
   private SpaceQuotaSnapshotNotifier spaceQuotaSnapshotNotifier;
@@ -1138,6 +1140,7 @@ public class HMaster extends HRegionServer implements MasterServices {
     configurationManager.registerObserver(this.cleanerPool);
     configurationManager.registerObserver(this.hfileCleaner);
     configurationManager.registerObserver(this.logCleaner);
+    configurationManager.registerObserver(this.regionsRecoveryConfigManager);
     // Set master as 'initialized'.
     setInitialized(true);
     assignmentManager.checkIfShouldMoveSystemRegionAsync();
@@ -1471,6 +1474,8 @@ public class HMaster extends HRegionServer implements MasterServices {
           "Provide threshold value > 0 for {} to enable it.",
         HConstants.STORE_FILE_REF_COUNT_THRESHOLD);
     }
+
+    this.regionsRecoveryConfigManager = new RegionsRecoveryConfigManager(this);
 
     replicationBarrierCleaner = new ReplicationBarrierCleaner(conf, this, getConnection(),
       replicationPeerManager);
