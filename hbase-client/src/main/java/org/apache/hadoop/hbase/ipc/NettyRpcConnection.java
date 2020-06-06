046c3499821e1332ea26d501095c413cc813f458
Duo Zhang <zhangduo@apache.org>
2020-06-06 21:11:49 +0800

    HBASE-24506 async client deadlock

--- hbase-client/src/main/java/org/apache/hadoop/hbase/ipc/NettyRpcConnection.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/ipc/NettyRpcConnection.java
@@ -22,10 +22,24 @@ import static org.apache.hadoop.hbase.ipc.CallEvent.Type.TIMEOUT;
 import static org.apache.hadoop.hbase.ipc.IPCUtil.setCancelled;
 import static org.apache.hadoop.hbase.ipc.IPCUtil.toIOE;
 
-import org.apache.hbase.thirdparty.io.netty.handler.timeout.ReadTimeoutHandler;
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.lang3.mutable.MutableInt;
+import org.apache.hadoop.hbase.ipc.BufferCallBeforeInitHandler.BufferCallEvent;
+import org.apache.hadoop.hbase.ipc.HBaseRpcController.CancellationCallback;
 import org.apache.hadoop.hbase.security.NettyHBaseRpcConnectionHeaderHandler;
-import org.apache.hbase.thirdparty.com.google.protobuf.RpcCallback;
+import org.apache.hadoop.hbase.security.NettyHBaseSaslRpcClientHandler;
+import org.apache.hadoop.hbase.security.SaslChallengeDecoder;
+import org.apache.hadoop.hbase.util.Threads;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.yetus.audience.InterfaceAudience;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import org.apache.hbase.thirdparty.com.google.protobuf.RpcCallback;
 import org.apache.hbase.thirdparty.io.netty.bootstrap.Bootstrap;
 import org.apache.hbase.thirdparty.io.netty.buffer.ByteBuf;
 import org.apache.hbase.thirdparty.io.netty.buffer.ByteBufOutputStream;
@@ -36,35 +50,27 @@ import org.apache.hbase.thirdparty.io.netty.channel.ChannelFutureListener;
 import org.apache.hbase.thirdparty.io.netty.channel.ChannelHandler;
 import org.apache.hbase.thirdparty.io.netty.channel.ChannelOption;
 import org.apache.hbase.thirdparty.io.netty.channel.ChannelPipeline;
+import org.apache.hbase.thirdparty.io.netty.channel.EventLoop;
 import org.apache.hbase.thirdparty.io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import org.apache.hbase.thirdparty.io.netty.handler.timeout.IdleStateHandler;
+import org.apache.hbase.thirdparty.io.netty.handler.timeout.ReadTimeoutHandler;
 import org.apache.hbase.thirdparty.io.netty.util.ReferenceCountUtil;
+import org.apache.hbase.thirdparty.io.netty.util.concurrent.FastThreadLocal;
 import org.apache.hbase.thirdparty.io.netty.util.concurrent.Future;
 import org.apache.hbase.thirdparty.io.netty.util.concurrent.FutureListener;
 import org.apache.hbase.thirdparty.io.netty.util.concurrent.Promise;
 
-import java.io.IOException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.yetus.audience.InterfaceAudience;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.apache.hadoop.hbase.ipc.BufferCallBeforeInitHandler.BufferCallEvent;
-import org.apache.hadoop.hbase.ipc.HBaseRpcController.CancellationCallback;
 import org.apache.hadoop.hbase.shaded.protobuf.generated.RPCProtos.ConnectionHeader;
-import org.apache.hadoop.hbase.security.NettyHBaseSaslRpcClientHandler;
-import org.apache.hadoop.hbase.security.SaslChallengeDecoder;
-import org.apache.hadoop.hbase.util.Threads;
-import org.apache.hadoop.security.UserGroupInformation;
 
 /**
  * RPC connection implementation based on netty.
- * <p>
+ * <p/>
  * Most operations are executed in handlers. Netty handler is always executed in the same
  * thread(EventLoop) so no lock is needed.
+ * <p/>
+ * <strong>Implementation assumptions:</strong> All the private methods except
+ * {@link #execute(Runnable)}, should all be called in the {@link #eventLoop} thread, otherwise
+ * there will be races.
  * @since 2.0.0
  */
 @InterfaceAudience.Private
@@ -73,40 +79,77 @@ class NettyRpcConnection extends RpcConnection {
   private static final Logger LOG = LoggerFactory.getLogger(NettyRpcConnection.class);
 
   private static final ScheduledExecutorService RELOGIN_EXECUTOR =
-      Executors.newSingleThreadScheduledExecutor(Threads.newDaemonThreadFactory("Relogin"));
+    Executors.newSingleThreadScheduledExecutor(Threads.newDaemonThreadFactory("Relogin"));
 
   private final NettyRpcClient rpcClient;
+
+  // the event loop used to set up the connection, we will also execute other operations for this
+  // connection in this event loop, to avoid locking everywhere.
+  private final EventLoop eventLoop;
 
   private ByteBuf connectionHeaderPreamble;
 
   private ByteBuf connectionHeaderWithLength;
 
-  @edu.umd.cs.findbugs.annotations.SuppressWarnings(value = "IS2_INCONSISTENT_SYNC",
-      justification = "connect is also under lock as notifyOnCancel will call our action directly")
-  private Channel channel;
+  // make it volatile so in the isActive method below we do not need to switch to the event loop
+  // thread to access this field.
+  private volatile Channel channel;
 
   NettyRpcConnection(NettyRpcClient rpcClient, ConnectionId remoteId) throws IOException {
     super(rpcClient.conf, AbstractRpcClient.WHEEL_TIMER, remoteId, rpcClient.clusterId,
-        rpcClient.userProvider.isHBaseSecurityEnabled(), rpcClient.codec, rpcClient.compressor);
+      rpcClient.userProvider.isHBaseSecurityEnabled(), rpcClient.codec, rpcClient.compressor);
     this.rpcClient = rpcClient;
+    this.eventLoop = rpcClient.group.next();
     byte[] connectionHeaderPreamble = getConnectionHeaderPreamble();
     this.connectionHeaderPreamble =
-        Unpooled.directBuffer(connectionHeaderPreamble.length).writeBytes(connectionHeaderPreamble);
+      Unpooled.directBuffer(connectionHeaderPreamble.length).writeBytes(connectionHeaderPreamble);
     ConnectionHeader header = getConnectionHeader();
     this.connectionHeaderWithLength = Unpooled.directBuffer(4 + header.getSerializedSize());
     this.connectionHeaderWithLength.writeInt(header.getSerializedSize());
     header.writeTo(new ByteBufOutputStream(this.connectionHeaderWithLength));
   }
 
-  @Override
-  protected synchronized void callTimeout(Call call) {
-    if (channel != null) {
-      channel.pipeline().fireUserEventTriggered(new CallEvent(TIMEOUT, call));
+  private static final FastThreadLocal<MutableInt> DEPTH = new FastThreadLocal<MutableInt>() {
+
+    @Override
+    protected MutableInt initialValue() throws Exception {
+      return new MutableInt(0);
+    }
+  };
+
+  private static final int MAX_DEPTH = 4;
+
+  private void execute(Runnable action) {
+    if (eventLoop.inEventLoop()) {
+      // this is used to prevent stack overflow, you can see the same trick in netty's LocalChannel
+      // implementation.
+      MutableInt depth = DEPTH.get();
+      if (depth.intValue() < MAX_DEPTH) {
+        depth.increment();
+        try {
+          action.run();
+        } finally {
+          depth.decrement();
+        }
+      } else {
+        eventLoop.execute(action);
+      }
+    } else {
+      eventLoop.execute(action);
     }
   }
 
   @Override
-  public synchronized boolean isActive() {
+  protected void callTimeout(Call call) {
+    execute(() -> {
+      if (channel != null) {
+        channel.pipeline().fireUserEventTriggered(new CallEvent(TIMEOUT, call));
+      }
+    });
+  }
+
+  @Override
+  public boolean isActive() {
     return channel != null;
   }
 
@@ -118,18 +161,22 @@ class NettyRpcConnection extends RpcConnection {
   }
 
   @Override
-  public synchronized void shutdown() {
-    shutdown0();
+  public void shutdown() {
+    execute(this::shutdown0);
   }
 
   @Override
-  public synchronized void cleanupConnection() {
-    if (connectionHeaderPreamble != null) {
-      ReferenceCountUtil.safeRelease(connectionHeaderPreamble);
-    }
-    if (connectionHeaderWithLength != null) {
-      ReferenceCountUtil.safeRelease(connectionHeaderWithLength);
-    }
+  public void cleanupConnection() {
+    execute(() -> {
+      if (connectionHeaderPreamble != null) {
+        ReferenceCountUtil.safeRelease(connectionHeaderPreamble);
+        connectionHeaderPreamble = null;
+      }
+      if (connectionHeaderWithLength != null) {
+        ReferenceCountUtil.safeRelease(connectionHeaderWithLength);
+        connectionHeaderWithLength = null;
+      }
+    });
   }
 
   private void established(Channel ch) throws IOException {
@@ -149,37 +196,32 @@ class NettyRpcConnection extends RpcConnection {
     if (error instanceof FallbackDisallowedException) {
       return;
     }
-    synchronized (this) {
-      if (reloginInProgress) {
-        return;
-      }
-      reloginInProgress = true;
-      RELOGIN_EXECUTOR.schedule(new Runnable() {
-
-        @Override
-        public void run() {
-          try {
-            if (shouldAuthenticateOverKrb()) {
-              relogin();
-            }
-          } catch (IOException e) {
-            LOG.warn("Relogin failed", e);
-          }
-          synchronized (this) {
-            reloginInProgress = false;
-          }
-        }
-      }, ThreadLocalRandom.current().nextInt(reloginMaxBackoff), TimeUnit.MILLISECONDS);
+    if (reloginInProgress) {
+      return;
     }
+    reloginInProgress = true;
+    RELOGIN_EXECUTOR.schedule(new Runnable() {
+
+      @Override
+      public void run() {
+        try {
+          if (shouldAuthenticateOverKrb()) {
+            relogin();
+          }
+        } catch (IOException e) {
+          LOG.warn("Relogin failed", e);
+        }
+        eventLoop.execute(() -> {
+          reloginInProgress = false;
+        });
+      }
+    }, ThreadLocalRandom.current().nextInt(reloginMaxBackoff), TimeUnit.MILLISECONDS);
   }
 
   private void failInit(Channel ch, IOException e) {
-    synchronized (this) {
-      // fail all pending calls
-      ch.pipeline().fireUserEventTriggered(BufferCallEvent.fail(e));
-      shutdown0();
-      return;
-    }
+    // fail all pending calls
+    ch.pipeline().fireUserEventTriggered(BufferCallEvent.fail(e));
+    shutdown0();
   }
 
   private void saslNegotiate(final Channel ch) {
@@ -212,7 +254,7 @@ class NettyRpcConnection extends RpcConnection {
             Promise<Boolean> connectionHeaderPromise = ch.eventLoop().newPromise();
             // create the handler to handle the connection header
             ChannelHandler chHandler = new NettyHBaseRpcConnectionHeaderHandler(
-                connectionHeaderPromise, conf, connectionHeaderWithLength);
+              connectionHeaderPromise, conf, connectionHeaderWithLength);
 
             // add ReadTimeoutHandler to deal with server doesn't response connection header
             // because of the different configuration in client side and server side
@@ -253,50 +295,34 @@ class NettyRpcConnection extends RpcConnection {
   private void connect() {
     LOG.trace("Connecting to {}", remoteId.address);
 
-    this.channel = new Bootstrap().group(rpcClient.group).channel(rpcClient.channelClass)
-        .option(ChannelOption.TCP_NODELAY, rpcClient.isTcpNoDelay())
-        .option(ChannelOption.SO_KEEPALIVE, rpcClient.tcpKeepAlive)
-        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, rpcClient.connectTO)
-        .handler(new BufferCallBeforeInitHandler()).localAddress(rpcClient.localAddr)
-        .remoteAddress(remoteId.address).connect().addListener(new ChannelFutureListener() {
+    this.channel = new Bootstrap().group(eventLoop).channel(rpcClient.channelClass)
+      .option(ChannelOption.TCP_NODELAY, rpcClient.isTcpNoDelay())
+      .option(ChannelOption.SO_KEEPALIVE, rpcClient.tcpKeepAlive)
+      .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, rpcClient.connectTO)
+      .handler(new BufferCallBeforeInitHandler()).localAddress(rpcClient.localAddr)
+      .remoteAddress(remoteId.address).connect().addListener(new ChannelFutureListener() {
 
-          @Override
-          public void operationComplete(ChannelFuture future) throws Exception {
-            Channel ch = future.channel();
-            if (!future.isSuccess()) {
-              failInit(ch, toIOE(future.cause()));
-              rpcClient.failedServers.addToFailedServers(remoteId.address, future.cause());
-              return;
-            }
-            ch.writeAndFlush(connectionHeaderPreamble.retainedDuplicate());
-            if (useSasl) {
-              saslNegotiate(ch);
-            } else {
-              // send the connection header to server
-              ch.write(connectionHeaderWithLength.retainedDuplicate());
-              established(ch);
-            }
+        @Override
+        public void operationComplete(ChannelFuture future) throws Exception {
+          Channel ch = future.channel();
+          if (!future.isSuccess()) {
+            failInit(ch, toIOE(future.cause()));
+            rpcClient.failedServers.addToFailedServers(remoteId.address, future.cause());
+            return;
           }
-        }).channel();
-  }
-
-  private void write(Channel ch, final Call call) {
-    ch.writeAndFlush(call).addListener(new ChannelFutureListener() {
-
-      @Override
-      public void operationComplete(ChannelFuture future) throws Exception {
-        // Fail the call if we failed to write it out. This usually because the channel is
-        // closed. This is needed because we may shutdown the channel inside event loop and
-        // there may still be some pending calls in the event loop queue after us.
-        if (!future.isSuccess()) {
-          call.setException(toIOE(future.cause()));
+          ch.writeAndFlush(connectionHeaderPreamble.retainedDuplicate());
+          if (useSasl) {
+            saslNegotiate(ch);
+          } else {
+            // send the connection header to server
+            ch.write(connectionHeaderWithLength.retainedDuplicate());
+            established(ch);
+          }
         }
-      }
-    });
+      }).channel();
   }
 
-  @Override
-  public synchronized void sendRequest(final Call call, HBaseRpcController hrc) throws IOException {
+  private void sendRequest0(Call call, HBaseRpcController hrc) throws IOException {
     if (reloginInProgress) {
       throw new IOException("Can not send request because relogin is in progress.");
     }
@@ -305,10 +331,8 @@ class NettyRpcConnection extends RpcConnection {
       @Override
       public void run(Object parameter) {
         setCancelled(call);
-        synchronized (this) {
-          if (channel != null) {
-            channel.pipeline().fireUserEventTriggered(new CallEvent(CANCELLED, call));
-          }
+        if (channel != null) {
+          channel.pipeline().fireUserEventTriggered(new CallEvent(CANCELLED, call));
         }
       }
     }, new CancellationCallback() {
@@ -322,30 +346,30 @@ class NettyRpcConnection extends RpcConnection {
             connect();
           }
           scheduleTimeoutTask(call);
-          final Channel ch = channel;
-          // We must move the whole writeAndFlush call inside event loop otherwise there will be a
-          // race condition.
-          // In netty's DefaultChannelPipeline, it will find the first outbound handler in the
-          // current thread and then schedule a task to event loop which will start the process from
-          // that outbound handler. It is possible that the first handler is
-          // BufferCallBeforeInitHandler when we call writeAndFlush here, but the connection is set
-          // up at the same time so in the event loop thread we remove the
-          // BufferCallBeforeInitHandler, and then our writeAndFlush task comes, still calls the
-          // write method of BufferCallBeforeInitHandler.
-          // This may be considered as a bug of netty, but anyway there is a work around so let's
-          // fix it by ourselves first.
-          if (ch.eventLoop().inEventLoop()) {
-            write(ch, call);
-          } else {
-            ch.eventLoop().execute(new Runnable() {
+          channel.writeAndFlush(call).addListener(new ChannelFutureListener() {
 
-              @Override
-              public void run() {
-                write(ch, call);
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+              // Fail the call if we failed to write it out. This usually because the channel is
+              // closed. This is needed because we may shutdown the channel inside event loop and
+              // there may still be some pending calls in the event loop queue after us.
+              if (!future.isSuccess()) {
+                call.setException(toIOE(future.cause()));
               }
-            });
-          }
+            }
+          });
         }
+      }
+    });
+  }
+
+  @Override
+  public void sendRequest(final Call call, HBaseRpcController hrc) {
+    execute(() -> {
+      try {
+        sendRequest0(call, hrc);
+      } catch (Exception e) {
+        call.setException(toIOE(e));
       }
     });
   }
