39d898195eb26f90cdc7f7829a4c0092817ff47c
Toshihiro Suzuki <brfrn169@gmail.com>
2020-06-12 18:59:03 +0900

    HBASE-24529 hbase.rs.evictblocksonclose is not honored when removing compacted files and closing the storefiles (#1881)
    Signed-off-by: Anoop Sam John <anoop.hbase@gmail.com>

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HStore.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/HStore.java
@@ -637,7 +637,8 @@ public class HStore implements Store, HeapSize, StoreConfigInformation,
       for (HStoreFile storeFile : results) {
         if (compactedStoreFiles.contains(storeFile.getPath().getName())) {
           LOG.warn("Clearing the compacted storefile {} from {}", storeFile, this);
-          storeFile.getReader().close(true);
+          storeFile.getReader().close(storeFile.getCacheConf() != null ?
+            storeFile.getCacheConf().shouldEvictOnClose() : true);
           filesToRemove.add(storeFile);
         }
       }
@@ -971,7 +972,8 @@ public class HStore implements Store, HeapSize, StoreConfigInformation,
           storeEngine.getStoreFileManager().clearCompactedFiles();
       // clear the compacted files
       if (CollectionUtils.isNotEmpty(compactedfiles)) {
-        removeCompactedfiles(compactedfiles);
+        removeCompactedfiles(compactedfiles, cacheConf != null ?
+          cacheConf.shouldEvictOnClose() : true);
       }
       if (!result.isEmpty()) {
         // initialize the thread pool for closing store files in parallel.
@@ -2735,7 +2737,7 @@ public class HStore implements Store, HeapSize, StoreConfigInformation,
         lock.readLock().unlock();
       }
       if (CollectionUtils.isNotEmpty(copyCompactedfiles)) {
-        removeCompactedfiles(copyCompactedfiles);
+        removeCompactedfiles(copyCompactedfiles, true);
       }
     } finally {
       archiveLock.unlock();
@@ -2745,8 +2747,10 @@ public class HStore implements Store, HeapSize, StoreConfigInformation,
   /**
    * Archives and removes the compacted files
    * @param compactedfiles The compacted files in this store that are not active in reads
+   * @param evictOnClose true if blocks should be evicted from the cache when an HFile reader is
+   *   closed, false if not
    */
-  private void removeCompactedfiles(Collection<HStoreFile> compactedfiles)
+  private void removeCompactedfiles(Collection<HStoreFile> compactedfiles, boolean evictOnClose)
       throws IOException {
     final List<HStoreFile> filesToRemove = new ArrayList<>(compactedfiles.size());
     final List<Long> storeFileSizes = new ArrayList<>(compactedfiles.size());
@@ -2771,7 +2775,7 @@ public class HStore implements Store, HeapSize, StoreConfigInformation,
             LOG.trace("Closing and archiving the file {}", file);
             // Copy the file size before closing the reader
             final long length = r.length();
-            r.close(true);
+            r.close(evictOnClose);
             // Just close and return
             filesToRemove.add(file);
             // Only add the length if we successfully added the file to `filesToRemove`
