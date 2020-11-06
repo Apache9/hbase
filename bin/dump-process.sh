5952495fee3e167f3b9dfa4db5d3bcdc2d988070
Guanghao Zhang <zhangguanghao1@xiaomi.com>
2020-11-06 14:59:01 +0800

    Support use async-profiler to generate flame graph

--- bin/dump-process.sh
+++ bin/dump-process.sh
@@ -21,6 +21,7 @@ then
     echo "Start jstack $i times"
     jstack $pid > $dir/$pid.jstack.$i
   done
+
   echo -e "\nDump ps with threads info to $dir/$pid.ps"
   ps -mp $pid -o THREAD,tid,time | awk '{if(NR>1)print}' | sort -k2 -n > $dir/$pid.ps
   echo -e "Find the top 10 cpu threads:\n"
@@ -29,6 +30,16 @@ then
   do
     grep $line $dir/$pid.jstack.0
   done
+
+  echo -e "\nStart using async-profiler to profile cpu 60 seconds"
+  curdir=$(pwd)
+  /opt/soft/async-profiler/profiler.sh -e cpu -d 60 -o svg -f $curdir/$dir/async-profiler-cpu.svg $pid
+  echo -e "Finished profile and save result to $dir/async-profiler-cpu.svg\n"
+
+  echo -e "Try the following cmd:\n"
+  echo "cd $dir"
+  echo "python -m SimpleHTTPServer"
+  echo -e "\nThen you can check the flame graph from http://$(hostname):8000/async-profiler-cpu.svg"
 else
   echo "There are no process $pid"
 fi
